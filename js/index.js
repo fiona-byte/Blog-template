394d3d6564f2fd219a48171a9f81bef4872ab92e
devfiona <54639346+fiona-byte@users.noreply.github.com>
2023-01-16 22:47:50 +0100

    Filter functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const hamburgerIcon = document.querySelector('.navigation__hamburger');
 const closeIcon = document.querySelector('.navigation__close');
 const navigationItem = document.querySelector('.navigation__container');
+const postsFilter = document.querySelector('.posts-filter__container');
+
+//  Navigation
 
 hamburgerIcon.addEventListener('click', () => {
   navigationItem.classList.add('open');
@@ -9,3 +12,13 @@ hamburgerIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   navigationItem.classList.remove('open');
 });
+
+// Filter
+
+function handleScrollRight() {
+  postsFilter.scrollBy({ left: 100, behavior: 'smooth' });
+}
+
+function handleScrollLeft() {
+  postsFilter.scrollBy({ left: -100, behavior: 'smooth' });
+}
